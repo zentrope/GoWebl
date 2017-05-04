09c564a3bf799be7d3e146efb81023e9c0386951
Keith Irwin <keith@zentrope.com>
2017-05-04 16:42:09 -0700

    Add routing to admin app, more placeholders.
    Signed-off-by: Keith Irwin <keith@zentrope.com>

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,12 +1,61 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
+
+class Posts extends React.PureComponent {
+
+  render() {
+    return (
+      <section>
+        <h1>Posts</h1>
+        <Link to="/admin/home">Home</Link>
+      </section>
+    )
+  }
+}
+
+class Authors extends React.PureComponent {
+  render() {
+    return (
+      <section>
+        <h1>Authors</h1>
+        <Link to="/admin/home">Home</Link>
+      </section>
+    )
+  }
+}
+
+class Home extends React.PureComponent {
+  render() {
+    const { endpoint } = this.props
+    return (
+      <div>
+        <h1>Placeholder ({endpoint})</h1>
+        <ul>
+          <li><Link to="/admin/post">Posts</Link></li>
+          <li><Link to="/admin/authors">Authors</Link></li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+const PropsRoute = ({component: Component, path: Path, ...rest}) => (
+  <Route path={Path} exact render={(props) => (<Component {...rest} {...props}/> )}/>
+)
 
 class App extends React.PureComponent {
   render() {
+    const { endpoint } = this.props
     return (
-      <div>
-        <h1>Placeholder</h1>
-        <a href="/">Home</a>
-      </div>
+      <Router>
+        <section>
+          <PropsRoute path="/" component={Redirect} to="/admin/home" endpoint={endpoint}/>
+          <PropsRoute path="/admin" component={Redirect} to="/admin/home" endpoint={endpoint}/>
+          <PropsRoute path="/admin/home" component={Home} endpoint={endpoint}/>
+          <PropsRoute path="/admin/post" component={Posts}/>
+          <PropsRoute path="/admin/authors" component={Authors}/>
+        </section>
+      </Router>
     );
   }
 }
--- admin/src/index.js
+++ admin/src/index.js
@@ -1,9 +1,29 @@
+// Copyright 2017 Keith Irwin. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+const isDevMode = window.location.port === "3000"
+const endpoint = isDevMode ? (
+  window.location.origin.replace("3000", "8080")
+) : (
+  window.location.origin
+)
+
+const docRoot =
   document.getElementById('root')
-);
+
+const render = () =>
+  ReactDOM.render(<App endpoint={endpoint}/>, docRoot)
+
+const main = () => {
+  console.log("Welcome to Webl Admin App")
+  console.log("Using '" + endpoint + "'.")
+  render()
+}
+
+window.onload = main
