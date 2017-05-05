4ba14e519e472ba132e213eba0c5ae5815893fa3
Keith Irwin <keith@zentrope.com>
2017-05-05 16:03:46 -0700

    Fix potential race condition.
    Signed-off-by: Keith Irwin <keith@zentrope.com>

--- internal/public.go
+++ internal/public.go
@@ -81,13 +81,13 @@ func isIndexPath(prefix string, r *http.Request) bool {
 
 func QueryAPI(api *GraphAPI) http.HandlerFunc {
 
-	var params struct {
-		Query         string                 `json:"query"`
-		OperationName string                 `json:"operationName"`
-		Variables     map[string]interface{} `json:"variables"`
-	}
-
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		var params struct {
+			Query         string                 `json:"query"`
+			OperationName string                 `json:"operationName"`
+			Variables     map[string]interface{} `json:"variables"`
+		}
 
 		if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
