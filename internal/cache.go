d5a10117980f6d522c6fd2a66ae9f94d51dd37ae
Keith Irwin <keith@zentrope.com>
2017-07-20 11:23:11 -0700

    Add naive thread-safe DNS cache protection.
    Signed-off-by: Keith Irwin <keith@zentrope.com>

--- internal/cache.go
+++ internal/cache.go
@@ -4,20 +4,28 @@
 
 package internal
 
-import "net"
+import (
+	"net"
+	"sync"
+)
 
+// alternative: https://github.com/orcaman/concurrent-map
 var DNS_CACHE = make(map[string]string, 0)
+var DNS_MUTEX = sync.Mutex{}
 
 // TODO: thread safe
 // TODO: go thread to age out values
 func DNSLookup(address string) string {
 
-	if name, found := DNS_CACHE[address]; found {
-		return name
-	}
-
 	if address == "0.0.0.0" {
 		return address
+	}
+
+	DNS_MUTEX.Lock()
+	defer DNS_MUTEX.Unlock()
+
+	if name, found := DNS_CACHE[address]; found {
+		return name
 	}
 
 	names, err := net.LookupAddr(address)
@@ -31,9 +39,6 @@ func DNSLookup(address string) string {
 		return address
 	}
 
-	name := names[0]
-
-	DNS_CACHE[address] = name
-
-	return name
+	DNS_CACHE[address] = names[0]
+	return names[0]
 }
